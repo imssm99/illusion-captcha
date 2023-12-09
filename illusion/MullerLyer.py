dd9a7a3b4397346ca0f757f8f79118c11193d8e9
imssm99 <imssm99@gmail.com>
2023-12-09 09:15:44 +0900

    refactor: N canvas

--- illusion/MullerLyer.py
+++ illusion/MullerLyer.py
@@ -16,17 +16,19 @@ class MullerLyer(BaseIllusion):
         pt1 = (0.3, 0.5)
         pt2 = (0.7, 0.5)
 
-        self.draw_line_relative(self.canvas[self.CORRECT], pt1, pt2, color[0], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], pt1, (pt1[0] - 0.1, pt1[1] + 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], pt1, (pt1[0] - 0.1, pt1[1] - 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], pt2, (pt2[0] + 0.1, pt2[1] + 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], pt2, (pt2[0] + 0.1, pt2[1] - 0.1), color[1], 0.01)
+        self.answer = 0
 
-        self.draw_line_relative(self.canvas[self.WRONG], pt1, pt2, color[0], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], pt1, (pt1[0] + 0.1, pt1[1] + 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], pt1, (pt1[0] + 0.1, pt1[1] - 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], pt2, (pt2[0] - 0.1, pt2[1] + 0.1), color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], pt2, (pt2[0] - 0.1, pt2[1] - 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], pt1, pt2, color[0], 0.01)
+        self.draw_line_relative(self.canvas[0], pt1, (pt1[0] - 0.1, pt1[1] + 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], pt1, (pt1[0] - 0.1, pt1[1] - 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], pt2, (pt2[0] + 0.1, pt2[1] + 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], pt2, (pt2[0] + 0.1, pt2[1] - 0.1), color[1], 0.01)
+
+        self.draw_line_relative(self.canvas[1], pt1, pt2, color[0], 0.01)
+        self.draw_line_relative(self.canvas[1], pt1, (pt1[0] + 0.1, pt1[1] + 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[1], pt1, (pt1[0] + 0.1, pt1[1] - 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[1], pt2, (pt2[0] - 0.1, pt2[1] + 0.1), color[1], 0.01)
+        self.draw_line_relative(self.canvas[1], pt2, (pt2[0] - 0.1, pt2[1] - 0.1), color[1], 0.01)
 
 if __name__ == "__main__":
     illusion = MullerLyer((400, 400))
--- illusion/Ponzo.py
+++ illusion/Ponzo.py
@@ -16,13 +16,15 @@ class Ponzo(BaseIllusion):
         line1 = [(0.4, 0), (0.1, 1)]
         line2 = [(0.6, 0), (0.9, 1)]
 
-        self.draw_line_relative(self.canvas[self.CORRECT], (0.35, 0.3), (0.65, 0.3), color[0], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], line1[0], line1[1], color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.CORRECT], line2[0], line2[1], color[1], 0.01)
+        self.answer = 0
 
-        self.draw_line_relative(self.canvas[self.WRONG], (0.35, 0.7), (0.65, 0.7), color[0], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], line1[0], line1[1], color[1], 0.01)
-        self.draw_line_relative(self.canvas[self.WRONG], line2[0], line2[1], color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], (0.35, 0.3), (0.65, 0.3), color[0], 0.01)
+        self.draw_line_relative(self.canvas[0], line1[0], line1[1], color[1], 0.01)
+        self.draw_line_relative(self.canvas[0], line2[0], line2[1], color[1], 0.01)
+
+        self.draw_line_relative(self.canvas[1], (0.35, 0.7), (0.65, 0.7), color[0], 0.01)
+        self.draw_line_relative(self.canvas[1], line1[0], line1[1], color[1], 0.01)
+        self.draw_line_relative(self.canvas[1], line2[0], line2[1], color[1], 0.01)
 
 if __name__ == "__main__":
     illusion = Ponzo((400, 400))
